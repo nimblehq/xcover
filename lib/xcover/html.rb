2eb8941f4a1076e6cbea8d102d8fc1ae7c9649b1
Rossukhon Leagmongkol <rose.roko@gmail.com>
2018-06-01 10:54:15 +0700

    Copy image assets for the styling

--- lib/xcover/html.rb
+++ lib/xcover/html.rb
@@ -1,11 +1,24 @@
 require 'erb'
+require 'fileutils'
 
 module Xcover
   class Html < Base
     def generate
       Dir.mkdir(output_dir) unless Dir.exist?(output_dir)
 
+      export_image_assets
+      export_report_page
+    end
+
+    private
+
+    def export_image_assets
+      FileUtils.cp_r("#{__dir__}/templates/images/", output_dir)
+    end
+
+    def export_report_page
       report = ERB.new(File.read("#{__dir__}/templates/html.erb")).result(binding)
+
       File.open(File.expand_path('index.html', Dir.open(output_dir)), 'w+') do |f|
         f.write report
       end
