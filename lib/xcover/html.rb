c678afd83f84a716146239920eb946421e546df4
Nikita Rodionov <>
2018-06-05 10:07:36 +0700

    Download display_logo if URL provided

--- lib/xcover/html.rb
+++ lib/xcover/html.rb
@@ -1,5 +1,7 @@
 require 'erb'
 require 'fileutils'
+require 'uri'
+require "open-uri"
 
 module Xcover
   class Html < Base
@@ -13,10 +15,16 @@ module Xcover
     private
 
     def export_assets
-      logo_file_path = File.expand_path(current_working_dir, display_logo)
-      FileUtils.cp(logo_file_path, output_dir) if display_logo && File.exist?(display_logo)
       FileUtils.cp("#{__dir__}/templates/styles.css", output_dir)
       FileUtils.cp_r("#{__dir__}/templates/images/", output_dir)
+
+      logo_file_path = "#{output_dir}/logo.png"
+
+      if File.exist?(display_logo)
+        FileUtils.cp(display_logo, logo_file_path)
+      elsif display_logo =~ URI::regexp
+        download_image(display_logo, logo_file_path)
+      end
     end
 
     def export_report_page
@@ -24,6 +32,12 @@ module Xcover
 
       File.open(File.expand_path('index.html', Dir.open(output_dir)), 'w+') do |f|
         f.write report
+      end
+    end
+
+    def download_image(url, dest)
+      open(url) do |u|
+        File.open(dest, 'wb') { |f| f.write(u.read) }
       end
     end
   end
